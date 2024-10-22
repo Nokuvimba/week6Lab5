fb9d735e3023b7322e7d0af68da8776e620c29bc
Nokuvimba <G00423951@atu.ie>
2024-10-22 11:41:57 +0100

    added vaidation for the product.

--- src/main/java/ie/atu/lab5week6/Product.java
+++ src/main/java/ie/atu/lab5week6/Product.java
@@ -1,5 +1,9 @@
 package ie.atu.lab5week6;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,8 +12,11 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Product {
+    @Min(value = 1,message = "id is at least 1")
     private int id;
+    @NotBlank(message = "name is not blank")
     private String name;
+    @Positive(message = "price is not  negative")
     private double price;
 
 }
--- src/main/java/ie/atu/lab5week6/ProductController.java
+++ src/main/java/ie/atu/lab5week6/ProductController.java
@@ -1,5 +1,6 @@
 package ie.atu.lab5week6;
 
+import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -17,11 +18,17 @@ public class ProductController {
     private List<Product> list =new ArrayList<>();
     //creating a class which is interested in requests and responses. Separation of Concern
     @PostMapping
-    public List<Product> newProduct( @RequestBody Product product){
+    public List<Product> newProduct(@Valid @RequestBody Product product){
         //send it to do business logic
        // ProductService myService = new ProductService();
         list = myService.addProduct(product);
         return  list;
     }
+   /*@PutMapping
+    public List<Product> updateProduct(@PathVariable("id")String id, @RequestBody Product product){
+        for(Product p: myService){
+
+        }
+    } */
 
 }
