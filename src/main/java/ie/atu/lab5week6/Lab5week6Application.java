6acaa1303a06616631b69067098ed900cdf93152
Nokuvimba <G00423951@atu.ie>
2024-10-22 10:13:37 +0100

    created project for week 6.

---
+++ src/main/java/ie/atu/lab5week6/Lab5week6Application.java
@@ -0,0 +1,13 @@
+package ie.atu.lab5week6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab5week6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab5week6Application.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/lab5week6/Lab5week6ApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.lab5week6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab5week6ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
